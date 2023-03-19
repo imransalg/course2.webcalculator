a89e118411530755ef0a8c5fcb1cae791bdd9bc4
imransalg <imransalgiriev5200@gmail.com>
2023-03-19 09:23:22 +0300

    web calculator done

--- src/main/java/pro/sky/java/course2/webcalculator/CalculatorController.java
+++ src/main/java/pro/sky/java/course2/webcalculator/CalculatorController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 @RequestMapping(path = "/calculator")
-
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
